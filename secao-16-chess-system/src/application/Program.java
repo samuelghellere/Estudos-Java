710784dead53b5fa06f053798cb38b03bea5d181
Samuel Ghellere <samuel.ghellere@gmail.com>
2021-06-07 07:57:21 -0300

    Seção 16 - Classes Board e UI

--- secao-16-chess-system/src/application/Program.java
+++ secao-16-chess-system/src/application/Program.java
@@ -1,13 +1,13 @@
 package application;
 
-import boardgame.Board;
+import chess.ChessMatch;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Board board = new Board(8, 8);
-				
-
+		ChessMatch chassmatch = new ChessMatch();
+		
+		UI.printBoard(chassmatch.getPieces());
 	}
 
 }
