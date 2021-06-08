69b1e061c2528160931b7485b871cb20dbd4d476
Samuel Ghellere <samuel.ghellere@gmail.com>
2021-06-08 07:17:04 -0300

    Seção 16 - Criação das classes King e Rook

--- secao-16-chess-system/src/chess/ChessMatch.java
+++ secao-16-chess-system/src/chess/ChessMatch.java
@@ -1,6 +1,11 @@
 package chess;
 
+import java.awt.Robot;
+
 import boardgame.Board;
+import boardgame.Position;
+import chess.pieces.King;
+import chess.pieces.Rook;
 
 public class ChessMatch {
 	
@@ -8,6 +13,7 @@ public class ChessMatch {
 	
 	public ChessMatch() {
 		this.board = new Board(8, 8);
+		initialStetup();
 	}
 	
 	public ChessPiece[][] getPieces(){
@@ -18,5 +24,11 @@ public class ChessMatch {
 			}
 		}
 		return mat;
+	}
+	
+	private void initialStetup() {
+		board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1));
+		board.placePiece(new King(board, Color.BLACK), new Position(0, 4));
+		board.placePiece(new King(board, Color.WHITE), new Position(7, 4));
 	}
 }
