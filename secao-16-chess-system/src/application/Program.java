2a7ed037e688723de50f82e8a956173d26ddea0e
Samuel Ghellere <samuel.ghellere@gmail.com>
2021-06-09 07:34:44 -0300

    Seção 16 - Limpar a tela e tratamento das execeções

--- secao-16-chess-system/src/application/Program.java
+++ secao-16-chess-system/src/application/Program.java
@@ -1,7 +1,9 @@
 package application;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
@@ -13,16 +15,26 @@ public class Program {
 		ChessMatch chassmatch = new ChessMatch();
 		
 		while (true) {
-			UI.printBoard(chassmatch.getPieces());
-			System.out.println();
-			System.out.print("Source: ");
+			try {
+					UI.clearScreen();
+					UI.printBoard(chassmatch.getPieces());
+					System.out.println();
+					System.out.print("Source: ");
+					
+					ChessPosition source = UI.readChessPosition(sc);
+					System.out.println();
+					System.out.print("Target: ");
+					ChessPosition target = UI.readChessPosition(sc);
+					
+					ChessPiece capturedPiece = chassmatch.performChessMove(source,target);
+				}catch(ChessException e) {
+					System.out.println(e.getMessage());
+					sc.nextLine();
+				}catch(InputMismatchException e) {
+					System.out.println(e.getMessage());
+					sc.nextLine();
+				}
 			
-			ChessPosition source = UI.readChessPosition(sc);
-			System.out.println();
-			System.out.print("Target: ");
-			ChessPosition target = UI.readChessPosition(sc);
-			
-			ChessPiece capturedPiece = chassmatch.performChessMove(source,target);
 		}
 	}
 
