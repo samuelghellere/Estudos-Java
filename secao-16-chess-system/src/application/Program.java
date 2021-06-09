3ac012b951adebdf03d0cdf9be3ea1b7d63c5485
Samuel Ghellere <samuel.ghellere@gmail.com>
2021-06-09 07:28:20 -0300

    Seção 16 - Logica de movimento das peças

--- secao-16-chess-system/src/application/Program.java
+++ secao-16-chess-system/src/application/Program.java
@@ -1,13 +1,29 @@
 package application;
 
+import java.util.Scanner;
+
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
 
 public class Program {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		ChessMatch chassmatch = new ChessMatch();
 		
-		UI.printBoard(chassmatch.getPieces());
+		while (true) {
+			UI.printBoard(chassmatch.getPieces());
+			System.out.println();
+			System.out.print("Source: ");
+			
+			ChessPosition source = UI.readChessPosition(sc);
+			System.out.println();
+			System.out.print("Target: ");
+			ChessPosition target = UI.readChessPosition(sc);
+			
+			ChessPiece capturedPiece = chassmatch.performChessMove(source,target);
+		}
 	}
 
 }
