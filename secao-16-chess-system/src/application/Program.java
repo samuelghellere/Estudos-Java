0626046ef5f1fab5a5b47fc2afbe0fcec4faf2a6
Samuel Ghellere <samuel.ghellere@gmail.com>
2021-06-10 07:36:12 -0300

    Seção 16 - Imprimindo movimentos possíveis

--- secao-16-chess-system/src/application/Program.java
+++ secao-16-chess-system/src/application/Program.java
@@ -12,21 +12,27 @@ public class Program {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		ChessMatch chassmatch = new ChessMatch();
+		ChessMatch chessmatch = new ChessMatch();
 		
 		while (true) {
 			try {
 					UI.clearScreen();
-					UI.printBoard(chassmatch.getPieces());
+					UI.printBoard(chessmatch.getPieces());
 					System.out.println();
 					System.out.print("Source: ");
 					
 					ChessPosition source = UI.readChessPosition(sc);
+					
+					boolean[][] possibleMoves = chessmatch.possibleMoves(source);
+					
+					UI.clearScreen();
+					UI.printBoard(chessmatch.getPieces(), possibleMoves);
+					
 					System.out.println();
 					System.out.print("Target: ");
 					ChessPosition target = UI.readChessPosition(sc);
 					
-					ChessPiece capturedPiece = chassmatch.performChessMove(source,target);
+					ChessPiece capturedPiece = chessmatch.performChessMove(source,target);
 				}catch(ChessException e) {
 					System.out.println(e.getMessage());
 					sc.nextLine();
